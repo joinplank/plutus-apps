4b5bfb0265f78361e971ebf77de57f34a2308cf6
valentino cerutti <valentinocerutti@gmail.com>
2022-07-13 20:19:56 -0300

    Replaces old utxosAt implementation

--- plutus-contract/src/Plutus/Contract/Request.hs
+++ plutus-contract/src/Plutus/Contract/Request.hs
@@ -45,7 +45,6 @@ module Plutus.Contract.Request(
     , utxoRefsAt
     , utxoRefsWithCurrency
     , utxosAt
-    , unspentTxOutsAt
     , utxosTxOutTxFromTx
     , utxosTxOutTxAt
     , txsFromTxIds
@@ -531,6 +530,7 @@ queryUnspentTxOutsAt addr pq = do
     r                           -> throwError $ review _ChainIndexContractError ("UnspentTxOutAtResponse", r)
 
 -- | Get the unspent transaction outputs at an address.
+-- WARNING: A maximum of 100 utxos can be gathered
 utxosAt ::
     forall w s e.
     ( AsContractError e
