36f1ab14d6c72fb00363e4468fa2a55f26261a7c
valentino cerutti <valentinocerutti@gmail.com>
2022-07-14 17:42:42 -0300

    Adds handler for UnspentTxOutSetAtAddress in Emulator
    Co-authored-by: Osi_Tomás <TomasOsiecki@users.noreply.github.com>

--- plutus-chain-index-core/src/Plutus/ChainIndex/Emulator/Handlers.hs
+++ plutus-chain-index-core/src/Plutus/ChainIndex/Emulator/Handlers.hs
@@ -289,7 +289,6 @@ handleQuery = \case
             _            -> pure $ TxosResponse page
     GetTip ->
         gets (tip . utxoState . view utxoIndex)
-    UnspentTxOutSetAtAddress _ _ -> error "OPERATION NOT SUPPORTED ON CHAIN INDEX"
 
 appendBlocks ::
     forall effs.
