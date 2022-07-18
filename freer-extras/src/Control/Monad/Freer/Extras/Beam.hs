d02aeeb5ca84fd781e3422df090b8102b8f14342
MartorSkull <martin_montane_6@hotmail.com>
2022-07-18 15:46:32 -0300

    WIP: Better type abstraction for Postgresql

--- freer-extras/src/Control/Monad/Freer/Extras/Beam.hs
+++ freer-extras/src/Control/Monad/Freer/Extras/Beam.hs
@@ -1,6 +1,8 @@
 
 module Control.Monad.Freer.Extras.Beam (
-      module Control.Monad.Freer.Extras.Beam.Postgres
+      module Control.Monad.Freer.Extras.Beam.Postgres,
+      module Control.Monad.Freer.Extras.Beam.Common
 ) where
 
+import Control.Monad.Freer.Extras.Beam.Common
 import Control.Monad.Freer.Extras.Beam.Postgres
