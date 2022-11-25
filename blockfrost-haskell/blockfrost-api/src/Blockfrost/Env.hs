4b5f2a5f170a2bf03a4c0e6e82a47b1c2f2bc804
Ale Gadea <ale@imagine.ai>
2022-11-25 18:55:26 -0300

    Adding local blockfrost haskell 0.4.0.1

---
+++ blockfrost-haskell/blockfrost-api/src/Blockfrost/Env.hs
@@ -0,0 +1,33 @@
+-- | Blockfrost environments
+
+module Blockfrost.Env
+  ( Env (..)
+  , parseEnv
+  ) where
+
+import Data.Text (Text)
+import Data.Text qualified
+import GHC.Generics (Generic)
+import Text.Read qualified
+
+-- | Blockfrost environments
+--
+-- Corresponds to Network when creating a Blockfrost project.
+-- Each environment has separate token.
+data Env =
+    Ipfs
+  | Mainnet
+  | Testnet
+  | Preprod
+  | Preview
+  | Localhost
+  deriving (Eq, Read, Show, Ord, Generic)
+
+-- | Try parsing @Env@ from @Text@
+parseEnv :: Text -> Either Text Env
+parseEnv tEnv = case Text.Read.readMaybe (Data.Text.unpack $ Data.Text.toTitle tEnv) of
+  Just env -> pure env
+  Nothing ->
+    Left
+      $ "Unknown environment: `" <> tEnv <> "`"
+      <> " expecting one of `ipfs`, `mainnet`, `testnet`, `localhost`"
